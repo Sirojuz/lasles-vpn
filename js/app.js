60ccc499ba60f9d2f6b7cc011a140498a852f55e
Sirojiddin Turopov <sirojiddinturopov707@gmail.com>
2023-04-15 00:45:19 +0500

    Media Fixsed

--- js/app.js
+++ js/app.js
@@ -7,7 +7,10 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:3
+            items:1
+        },
+        800:{
+            items:2
         },
         1000:{
             items:3
